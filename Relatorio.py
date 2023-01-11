1cb7e44873420c99296bd85acfbdcbe6ba5770ab
Leonardo Tricano <leonardot@dime.com>
2023-01-11 12:03:27 -0300

    Atualizando def dados

--- Relatorio.py
+++ Relatorio.py
@@ -1,10 +1,13 @@
 #video referecia:https://www.youtube.com/watch?v=TXlkiMIBlTM
+#Tentando fazer um programa para o salão de um amigo
 import tkinter as tk
 import datetime as dt
 from tkinter import ttk
 
-
+#Lista para o combobox (Testando sem banco de dados)
 lista_tipos=["Corte","Unha","Mechas"]
+lista_tipo_pagamento=["Dinheio","Pix","Credito","Debito"]
+lista_dados=[]
 
 janela = tk.Tk()
 def center(win):
@@ -35,32 +38,55 @@ def center(win):
     # after setting correct window position
     win.deiconify()
 
-janela.minsize(280,200)
+def inserindo_dados():
+    cliente = entry_cliente.get()
+    servicos = entry_servicos.get()
+    pagamento = entry_tipo_pagamento.get()
+    data_servico = dt.datetime.now()
+    data_servico = data_servico.strftime("%d/%m/%Y %H:%M")
+    dados = len(lista_dados)+1
+    dados_str = "DAD-{}".format(dados)
+    lista_dados.append((dados_str,cliente,servicos,pagamento,data_servico))
 
+#Tamaho tela (Depende do que você está fazendo recomendo ir testando
+janela.minsize(300,210)
 #Função para abrir no meio da janela
 center(janela)
 #Titulo
 janela.title('Serviços salão')
+
+
 #================================== Conteúdo programa ===================================
-label_descricao = tk.Label(text="Entre com os dados:")
+#Titulo
+label_descricao = tk.Label(text="Salão William")
 label_descricao.grid(row=1, column=0,padx=10,pady=10, stick='nswe',columnspan=4)
 
-entry_descricao = tk.Entry()
-entry_descricao.grid(row=2, column=0,padx=10,pady=10, stick='nswe',columnspan=4)
+#Titulo cliente
+label_cliente = tk.Label(text="Cliente")
+label_cliente.grid(row=2, column=0,padx=10,pady=10, stick='nswe',columnspan=2)
 
+#Entrada cliente
+entry_cliente = tk.Entry()
+entry_cliente.grid(row=2, column=2,padx=10,pady=10, stick='nswe',columnspan=2)
+
+#Titulos Serviços
 label_tipdo_servico = tk.Label(text="Tipo do serviço")
 label_tipdo_servico.grid(row=3, column=0,padx=10,pady=10, stick='nswe',columnspan=2)
 
-entry_servicos = ttk.Combobox(values=lista_tipos)
+#Combobox serviços
+entry_servicos = ttk.Combobox(values=lista_tipos) #Lista criada no início
 entry_servicos.grid(row=3, column=2,padx=10,pady=10, stick='nswe',columnspan=2)
 
-label_vezes=tk.Label(text="Servicos feitos")
-label_vezes.grid(row=4, column=0,padx=10,pady=10, stick='nswe',columnspan=2)
+#Titulo Pagamento
+label_tipo_pagamento = tk.Label(text="Tipo de pagamento")
+label_tipo_pagamento.grid(row=4, column=0,padx=10,pady=10, stick='nswe',columnspan=2)
 
-entry_vezes = tk.Entry()
-entry_vezes.grid(row=4, column=2,padx=10,pady=10, stick='nswe',columnspan=2)
+#Combobox tipo de pagamento
+entry_tipo_pagamento = ttk.Combobox(values=lista_tipo_pagamento) #Lista criada no início
+entry_tipo_pagamento.grid(row=4, column=2,padx=10,pady=10, stick='nswe',columnspan=2)
 
-botao_confirmar = tk.Button(text="Confirmar")
+
+botao_confirmar = tk.Button(text="Confirmar",command=inserindo_dados())
 botao_confirmar.grid(row=5, column=0,padx=10,pady=10, stick='nswe',columnspan=4)
 
 
